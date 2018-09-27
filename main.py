c6c0adb5f31f90d8029a9a733fb8f6fb3c268b1c
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2018-09-27 21:06:17 +0530

    API usage, commit to transfer code to lab

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-import sys
-from selenium import webdriver
+# import sys
+# from selenium import webdriver
 
-# To Add : Argument parse
-# To Add : Automation of QR entry post one session - Look into profile sessions
+# # To Add : Argument parse
+# # To Add : Automation of QR entry post one session - Look into profile sessions
 
-# options = webdriver.FirefoxOptions()
-# options.add_argument(r"/")
+# # options = webdriver.FirefoxOptions()
+# # options.add_argument(r"/")
 
-driver = webdriver.Firefox()
-driver.get('https://web.whatsapp.com')
+# driver = webdriver.Firefox()
+# driver.get('https://web.whatsapp.com')
 
-name = input("Enter name of contact : ")
-message = input("Enter message to send : ")
+# name = input("Enter name of contact : ")
+# message = input("Enter message to send : ")
 
-user = driver.find_elements_by_xpath('//span[@title = "{}"]'.format(name))
-user[0].click()
+# user = driver.find_elements_by_xpath('//span[@title = "{}"]'.format(name))
+# user[0].click()
 
-box = driver.find_element_by_class_name('_2S1VP')
-box.send_keys(message)
-send_button = driver.find_element_by_class_name('_35EW6')
-send_button.click() 
+# box = driver.find_element_by_class_name('_2S1VP')
+# box.send_keys(message)
+# send_button = driver.find_element_by_class_name('_35EW6')
+# send_button.click() 
+
+from webwhatsapi import WhatsAPIDriver
+driver = WhatsAPIDriver(username="kaushal")
+driver.get_qr()
