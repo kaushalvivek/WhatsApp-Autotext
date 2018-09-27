59dfd2b94b51939d1e1520fc526d1c4472c03441
kaushalvivek <vivek.kaushal@research.iiit.ac.in>
2018-09-27 22:17:32 +0530

    Persistent LogIn Added

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-# import sys
-# from selenium import webdriver
+import sys
+from selenium import webdriver
 
-# # To Add : Argument parse
-# # To Add : Automation of QR entry post one session - Look into profile sessions
+# To Add : Argument parse
 
-# # options = webdriver.FirefoxOptions()
-# # options.add_argument(r"/")
+profile = "/home/kaushal/.mozilla/firefox/zzhbpihh.default"
+fire_profile = webdriver.FirefoxProfile('/home/kaushal/.mozilla/firefox/zzhbpihh.default')
+driver = webdriver.Firefox(fire_profile)
+driver.get('https://web.whatsapp.com') 
 
-# driver = webdriver.Firefox()
-# driver.get('https://web.whatsapp.com')
+name = input("Enter name of contact : ")
+message = input("Enter message to send : ")
 
-# name = input("Enter name of contact : ")
-# message = input("Enter message to send : ")
+user = driver.find_elements_by_xpath('//span[@title = "{}"]'.format(name))
+user[0].click()
 
-# user = driver.find_elements_by_xpath('//span[@title = "{}"]'.format(name))
-# user[0].click()
+box = driver.find_element_by_class_name('_2S1VP')
+box.send_keys(message)
+send_button = driver.find_element_by_class_name('_35EW6')
+send_button.click()
 
-# box = driver.find_element_by_class_name('_2S1VP')
-# box.send_keys(message)
-# send_button = driver.find_element_by_class_name('_35EW6')
-# send_button.click() 
-
-from webwhatsapi import WhatsAPIDriver
-driver = WhatsAPIDriver(username="kaushal")
-driver.get_qr()
